1aee5afea3b19255355727e29087f894c044c68a
Epic91 <hernandeze791@gmail.com>
2021-02-01 19:55:14 -0600

    commit tree 23f441cfbefd34feae8b35837211a56bd70d2287

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+const timeStr = '19:00';
+function greet(timeStr) {
+  if (timeStr === '19:00') {
+    return "Good Evening";
+  }
+  if (timeStr > '08:00') {
+    return "Good Morning";
+  }
+  if (timeStr < '13:00') {
+    return "Good Afternoon";
+  }
+  return "Good Evening"
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
